67589992a4f2e00a770ebb958298e17933143e7f
TSRaghu <TSRaghu@MRYITDL18108.languageline.com>
2019-06-24 14:24:28 -0700

    0624 - Select_from_world
    Additional test cases for Select from world is added

--- Select_from_world.sql
+++ Select_from_world.sql
@@ -33,7 +33,41 @@
 	  FROM world
 	  WHERE name like '%united%'
 
+   /* 7.Show the countries that are big by area or big by population. Show name, population and area.*/
    
+      SELECT name, population, area
+	  FROM world
+	  WHERE area > 3000000
+	  OR population > 250000000
+	  
+   /* 8.Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
+	  
+	  -- Australia has a big area but a small population, it should be included.
+      -- Indonesia has a big population but a small area, it should be included.
+	  -- China has a big population and big area, it should be excluded.
+	  -- United Kingdom has a small population and a small area, it should be excluded.
+	  
+	  SELECT name, population,area
+  	  FROM world
+  	  WHERE
+  	  (population>250000000 OR area>3000000)
+  	  AND NOT(population>250000000 AND area>3000000)
    
+   /* 9.For South America show population in millions and GDP in billions both to 2 decimal places.
+   
+      SELECT name, ROUND(population/1000000,2),
+             ROUND(gdp/1000000000,2)
+  	  FROM world
+ 	  WHERE continent='South America'
+ 	  
+   /* 10.Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+    
+      SELECT name, ROUND(gdp/population,-3)
+  	  FROM world
+      WHERE
+      gdp>1000000000000
+
+   
+   	  
    
    
